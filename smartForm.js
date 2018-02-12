1a77f6c33fe4b3ccc435df6081736840657d972e
VladPerish <perish.abs@gmail.com>
2018-02-12 09:23:31 +0600

    half done

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,32 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData={};
+
+$('#start').click(function(){
+
+  console.log("hahaha");
+  $('#welcome').hide();
+  $('#q1').show();
+
+});
+
+$('#name').change(function(){
+  console.log($('#name').val());
+});
+
+$('#submitUserDetail').click(function(){
+  if ($('#name').val()  &&  $('#email').val()) {
+    userData.name=$('#name').val();
+    userData.email=$('#email').val();
+    if (true) {
+
+    }
+    $('#q2').show();
+    $('#q1').hide();
+  }
+  else {
+    alert('Please Enter a name and an email');
+  }
+
+
+});
